e8ccd79ba715520d8a7aefbae7a91f51baa89b35
Luca De Feo <defeo@pippin>
2019-01-22 01:26:26 +0100

    Added min poly

---
+++ implementation/minpoly.h
@@ -0,0 +1,11 @@
+#ifndef MINPOLY_H
+#define MINPOLY_H
+
+#include <flint/fq_nmod.h>
+#include <flint/nmod_poly_mat.h>
+
+void minpoly(nmod_poly_t result,
+	     const fq_nmod_t f, const fq_nmod_ctx_t ctx,
+ 	     flint_rand_t state);
+
+#endif
