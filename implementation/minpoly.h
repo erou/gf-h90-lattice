d21a8b8debb5418ac4af05651601c66e5db844f2
Luca De Feo <defeo@pippin>
2019-01-25 16:27:19 +0100

    Added routines for change of basis, automated Makefile

--- implementation/minpoly.h
+++ implementation/minpoly.h
@@ -4,6 +4,22 @@
 #include <flint/fq_nmod.h>
 #include <flint/nmod_poly_mat.h>
 
+/**
+ * Computes the power projection:
+ * \[\langle v, 1 \rangle, \langle v, h \rangle, \dots, \langle v, h^{l - 1} \rangle\]
+ * where $\langle ., . \rangle$ is the inner product, considering $h^i$ as the vector
+ * of its coefficients.
+ * 
+ * @param modulus_inv_rev	1 / rev(m + 1, modulus) mod x^{m - 1} where m = deg(modulus)
+ */
+void project_powers(mp_limb_t *result, const mp_limb_t *a, slong l, const nmod_poly_t h,
+		    const nmod_poly_t modulus, const nmod_poly_t modulus_inv_rev);
+
+/**
+ * Computes the minimal polynomial of {@code f} modulo {@code ctx}.
+ * 
+ * @param result	the minimal polynomial of {@code f}
+ */
 void minpoly(nmod_poly_t result,
 	     const fq_nmod_t f, const fq_nmod_ctx_t ctx);
 
